16e430a671a9191d5ff306af83283e1d53f27448
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-20 02:42:50 +0300

    Updated base

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,20 @@ from datetime import datetime
 
 class BaseModel:
     """Base class for the entire HBNB project"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new BaseModel instance
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.fromisoformat(value)
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f"[{self.__class__.__name__}] {self.id} {self.__dict__}"
