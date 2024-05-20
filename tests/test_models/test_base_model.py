3692c2d2ee4d573efe6580f9dc76735a8e7da4c2
BrianKip2000 <kipchirchirbrian559@gmail.com>
2024-05-20 15:42:53 +0300

    Thogh

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,8 +8,19 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
     """Test save() class"""
     def test_save_method(self):
+        """Method string to test for BaseModel saving"""
         saving_class = BaseModel()
         saving_class_new = saving_class.updated_at
         saving_class.save()
         self.assertNotEqual(saving_class_new, saving_class.updated_at)
         self.assertGreater(saving_class.updated_at, saving_class_new)
+
+    def test_to_str_method(self):
+        """Unittest for to string method"""
+        new_string = BaseModel()
+        new_string.name = "Brian"
+        new_string.my_number = 89
+        self.assertEqual(new_string_dict['__class__'], 'BaseModel')
+
+if __name__ == '__main__':
+    unittest.main()
