cc7a0f71fc29a3f677a2bab379d819eca7053b5e
BrianKip2000 <kipochirchirbrian559@gmail.com>
2024-06-02 14:49:41 +0300

    Commit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,34 +11,34 @@ class FileStorage:
 
     def all(self):
         """Returns the dictionary of objects"""
-        return type(self).__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Sets new obj in __objects dictionary."""
-        if obj.id in type(self).__objects:
+        if obj.id in FileStorage.__objects:
             print("exists")
             return
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        type(self).__objects[key] = obj
+        FileStorage.__objects[key] = obj
         # OR
         # type(self).__objects[obj.id] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         new_dict = []
-        for obj in type(self).__objects.values():
+        for obj in FileStorage.__objects.values():
             new_dict.append(obj.to_dict())
-        with open(type(self).__file_path, "w", encoding='utf-8') as file:
+        with open(FileStorage.__file_path, "w", encoding='utf-8') as file:
             json.dump(new_dict, file)
 
     def reload(self):
         """Deserializes the JSON file to __objects if it exists"""
-        if os.path.exists(type(self).__file_path) is True:
+        if os.path.exists(FileStorage.__file_path) is True:
             try:
-                with open(type(self).__file_path, "r") as file:
+                with open(FileStorage.__file_path, "r") as file:
                     new_obj = json.load(file)
                     for key, val in new_obj.items():
                         obj = self.class_dict[val['__class__']](**val)
-                        type(self).__objects[key] = obj
+                        FileStorage.__objects[key] = obj
             except Exception:
                 pass
